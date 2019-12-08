819915250684e675d65cf26bb628f3a899d5f0b9
mohamedelaraby <mohamedelaraby1995@gmail.com>
2019-12-09 00:42:31 +0200

    Fix admin home page design

---
+++ app/views/admin-header.php
@@ -0,0 +1,23 @@
+<!---
+[  author ] ;- Muhammed alaraby
+[  Date   ] ;- 5/12/19
+[  Info   ] ;-  Admin page header
+---->
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <!-- [ Style Sheets ] -->
+    <link rel="stylesheet" href="../public/vendors/css/bootstrap.min.css">
+    <link rel="stylesheet" href="../public/vendors/css/sb-admin.css">
+    <link href="../public/vendors/fontawesome/css/all.css" rel="stylesheet">
+    <!--load all styles -->
+    <!-- [ Website title ] -->
+    <title>CMS - a control managment system</title>
+
+</head>
+
+<body>
--- app/views/footer.php
+++ app/views/footer.php
@@ -3,12 +3,18 @@
 [  Date   ]  ;-  5/12/19
 [  Info   ]  ;-   Main Footer
 ---->
-    </div>
-<!-- Scripts --------------------------------------------------------------------------------->
-        <script src="../public/vendors/js/jquery-3.4.1.js"></script>
-        <script src="../public/vendors/js/popper.min.js"></script>
-        <script src="../public/vendors/js/bootstrap.min.js"></script>
-     
-        <!-- Fonts Awesome script -->
- <script defer src="../public/vendors/fontawesome/js/all.js"></script> <!--load all styles -->    </body>
-</html>
+
+
+
+
+   <!-- Scripts --------------------------------------------------------------------------------->
+   <script src="../public/vendors/js/jquery.js"></script>
+   <script src="../public/vendors/js/popper.min.js"></script>
+   <script src="../public/vendors/js/bootstrap.min.js"></script>
+
+   <!-- Fonts Awesome script -->
+   <script defer src="../public/vendors/fontawesome/js/all.js"></script>
+   <!--load all styles -->
+   </body>
+
+   </html>
